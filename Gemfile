5cf55ae9cb26681c0492b837585b941a3bd9384f
Adam Somora <Adam.Somora@4finance.com>
2020-05-07 01:57:37 +0200

    finalize main class with gems

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'descriptive_statistics'
+gem 'json'
+gem 'rspec'
+gem 'rubocop'
+gem 'thread'
--- lib/sensor_evaluator.rb
+++ lib/sensor_evaluator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'thread/pool'
+
 require_relative 'file_decorator'
 require_relative 'sensor'
 
@@ -9,11 +11,47 @@ class SensorEvaluator
     @log_contents_str = log_contents_str
     @sensors = %w[thermometer humidity monoxide]
     @log = FileDecorator.new(@log_contents_str, @sensors)
-    @sensor = Sensor.new
+    @threads = 5
   end
 
   def evaluate
+    analyze_sensors
+  end
+
+  def return_hash
     @log.to_hash
+  end
+
+  def build_sensor(sensor, reference)
+    new_sensor = Sensor.new
+
+    new_sensor.type = sensor[:type]
+    new_sensor.name = sensor[:name]
+    new_sensor.data = sensor[:data]
+    new_sensor.reference = reference
+    new_sensor
+  end
+
+  def analyze(sensor, reference)
+    new_sensor = build_sensor(sensor, reference)
+    new_sensor.analyze
+  end
+
+  def analyze_sensors
+    analyzed_hash = return_hash
+    final_hash = {}
+
+    # pool = Thread.pool(@threads)
+
+    analyzed_hash[:data].each do |sensor|
+      # pool.process do
+        final_hash[sensor[:name]] = analyze(sensor, analyzed_hash[:reference])
+      # end
+    end
+
+    # pool.shutdown
+
+    final_hash
   end
 
   # def process_log_hash
