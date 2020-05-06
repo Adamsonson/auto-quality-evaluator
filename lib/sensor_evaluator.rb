0cfc2a76c1aa0f1e0ba6f8844bbf6acf65600141
Adam Somora <Adam.Somora@4finance.com>
2020-05-06 17:25:49 +0200

    set basis for sensor evaluator class

--- lib/sensor_evaluator.rb
+++ lib/sensor_evaluator.rb
@@ -1,12 +1,43 @@
 # frozen_string_literal: true
 
-# Evaluates each sensor based on input
-class SensorEvaluation
+require_relative 'file_decorator'
+require_relative 'sensor'
+
+# Evaluates sensor log
+class SensorEvaluator
   def initialize(log_contents_str)
     @log_contents_str = log_contents_str
+    @sensors = %w[thermometer humidity monoxide]
+    @log = FileDecorator.new(@log_contents_str, @sensors)
+    @sensor = Sensor.new
   end
 
   def evaluate
-    'TBD'
+    @log.to_hash
   end
+
+  # def process_log_hash
+  #   @log.to_hash.each |sensor_logs| do
+  #     @sensor.analyse(sensor_logs)
+  #   end
+  # end
+
+  # pseudo code
+  # DONE 1. A. open file (in separate class) - FileReader
+  # DONE 1. B. process it to certain format (json)
+  # 2. process the log file (json) - (where threading might be included) with looping
+  # and processing each sensor in separate class SensorType (with another looping)
+  # 3. the output should be saving to certain hash - wheather within looping method or inside SensorType class (by global variable?)
+  # {
+  #   "temp-1": "precise",
+  #   "temp-2": "ultra precise",
+  #   "hum-1": "keep",
+  #   "hum-2": "discard",
+  #   "mon-1": "keep",
+  #   "mon-2": "discard"
+  #   }
+  #
+  # create simple rspec test based on text_analyzer
+    # - is output Hash?
+    # - use mockup data - should return certain output based on inserted mockup data
 end
