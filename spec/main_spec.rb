e1e2b7f97ef5ab76676dda9970c2a03da0293a05
Adam Somora <Adam.Somora@4finance.com>
2020-05-07 02:22:55 +0200

    implement couple of basic tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require_relative '../main.rb'
 
-# create simple rspec test based on text_analyzer
-# - is output Hash?
-# - use mockup data - should return certain output based on inserted mockup data
+describe '#analyze' do
+  it 'should return a hash' do
+    expect(execute).to be_a Hash
+  end
+
+  it 'should return evaluation hash for a simple log file' do
+    expect(execute).to eq({ 'temp-1' => 'precise',
+                            'temp-2' => 'ultra precise',
+                            'hum-1' => 'keep',
+                            'hum-2' => 'discard',
+                            'mon-1' => 'keep',
+                            'mon-2' => 'keep' })
+  end
+end
