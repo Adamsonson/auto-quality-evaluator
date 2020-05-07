b4e394750298dd8d24f8659458a8d60ff3eaf2b6
Adam Somora <Adam.Somora@4finance.com>
2020-05-07 02:03:40 +0200

    set-up file for rspec simple test

---
+++ spec/main_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+# create simple rspec test based on text_analyzer
+# - is output Hash?
+# - use mockup data - should return certain output based on inserted mockup data
