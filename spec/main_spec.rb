a0a49c63cdfdb4f613c81e061a5f7fbee96fe083
Adam Somora <Adam.Somora@4finance.com>
2020-05-07 13:41:53 +0200

    finalize simple tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,18 +1,35 @@
 # frozen_string_literal: true
 
-require_relative '../main.rb'
+require_relative '../lib/helper_methods.rb'
 
-describe '#analyze' do
+describe '#sensors_evaluation' do
   it 'should return a hash' do
-    expect(execute).to be_a Hash
+    expect(evaluate('test', 1)).to be_a Hash
   end
 
-  it 'should return evaluation hash for a simple log file' do
-    expect(execute).to eq({ 'temp-1' => 'precise',
-                            'temp-2' => 'ultra precise',
-                            'hum-1' => 'keep',
-                            'hum-2' => 'discard',
-                            'mon-1' => 'keep',
-                            'mon-2' => 'keep' })
+  context 'with sample log data input' do
+    let(:sensor) { Sensor.new }
+    let(:result) { 'precise' }
+    let(:name)   { 'temp-1' }
+    let(:type)   { 'thermometer' }
+    let(:reference) { { thermometer: '70.0' } }
+    let(:data) do
+      [{ timestamp: '2007-04-05T22:00', reading: '72.4' },
+       { timestamp: '2007-04-05T22:01', reading: '76.0' },
+       { timestamp: '2007-04-05T22:02', reading: '79.1' }]
+    end
+
+    it 'returns correct sensor evaluation' do
+      allow(sensor).to receive(:analyze)
+        .and_return(result)
+
+      analyse = Sensor.new
+      analyse.name = name
+      analyse.type = type
+      analyse.data = data
+      analyse.reference = reference
+
+      expect(analyse.analyze).to eq result
+    end
   end
 end
