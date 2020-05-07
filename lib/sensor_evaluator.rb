6b029c451774833330c9fe434297a507541c4e73
Adam Somora <Adam.Somora@4finance.com>
2020-05-07 02:03:16 +0200

    enable thread pooling

--- lib/sensor_evaluator.rb
+++ lib/sensor_evaluator.rb
@@ -41,41 +41,16 @@ class SensorEvaluator
     analyzed_hash = return_hash
     final_hash = {}
 
-    # pool = Thread.pool(@threads)
+    pool = Thread.pool(@threads)
 
     analyzed_hash[:data].each do |sensor|
-      # pool.process do
+      pool.process do
         final_hash[sensor[:name]] = analyze(sensor, analyzed_hash[:reference])
-      # end
+      end
     end
 
-    # pool.shutdown
+    pool.shutdown
 
     final_hash
   end
-
-  # def process_log_hash
-  #   @log.to_hash.each |sensor_logs| do
-  #     @sensor.analyse(sensor_logs)
-  #   end
-  # end
-
-  # pseudo code
-  # DONE 1. A. open file (in separate class) - FileReader
-  # DONE 1. B. process it to certain format (json)
-  # 2. process the log file (json) - (where threading might be included) with looping
-  # and processing each sensor in separate class SensorType (with another looping)
-  # 3. the output should be saving to certain hash - wheather within looping method or inside SensorType class (by global variable?)
-  # {
-  #   "temp-1": "precise",
-  #   "temp-2": "ultra precise",
-  #   "hum-1": "keep",
-  #   "hum-2": "discard",
-  #   "mon-1": "keep",
-  #   "mon-2": "discard"
-  #   }
-  #
-  # create simple rspec test based on text_analyzer
-    # - is output Hash?
-    # - use mockup data - should return certain output based on inserted mockup data
 end
